d0ab8a6667c07a39a4314e10efad623a1e2898af
Sajan Sunny <sajan.sunny@outlook.com>
2021-02-02 08:11:08 +0530

    new files added

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -4,13 +4,13 @@ from selenium import webdriver
 @pytest.fixture()
 def setup(browser):
     if browser == "chrome":
-        driver = webdriver.Chrome(executable_path=".\\Configurations\\Drivers\\chromedriver.exe")
+        driver = webdriver.Chrome()
         print("Launching Chrome browser..")
     elif browser == "firefox":
-        driver = webdriver.Firefox(executable_path=".\\Configurations\\Drivers\\geckodriver.exe")
+        driver = webdriver.Firefox()
         print("Launching Firefox browser..")
     else:
-        driver = webdriver.Ie(executable_path=".\\Configurations\\Drivers\\IEDriverServer.exe")
+        driver = webdriver.Ie()
         print("Launching IE browser..")
     return driver
 
